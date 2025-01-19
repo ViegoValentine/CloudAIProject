32bd8b2d399702f70f1e5c8b15a6885b09c927ee
Hubert <hubert.bojda12@gmail.com>
2025-01-19 13:24:39 +0100

    add notification and edit responces

--- CloudAIProject/Frontend/src/App.jsx
+++ CloudAIProject/Frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { ToastContainer } from "react-toastify";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Home from './Components/Home/Home'
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <div>
+      <ToastContainer />
       <Home />
     </div>
   )
